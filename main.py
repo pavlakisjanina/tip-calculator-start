37c6e7e8fe57c64e8621e3463ca0ec9d1848ccb3
pavlakisjanina <160166957+pavlakisjanina@users.noreply.github.com>
2024-07-09 21:48:55 +0000

    Resolved merge conflicts and updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 print("Welcome to the tip calculator")
+# User adds total cost of bill
 bill = float(input("What was the total bill? "))
+# User adds desired tip
 percentage = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+# User adds amount of peoples splitting the bill
 people = float(input("How many people to split the bill? "))
+# Calculates the total cost of the bill
 meal_with_tip = (bill/people) / 100 * percentage + bill/people
+# Rounds the total cost of the bill to 2 decimal places
 round(meal_with_tip, 2)
+# Prints the total cost of the bill to the user
 print(f"Each Person should pay: {meal_with_tip}")
