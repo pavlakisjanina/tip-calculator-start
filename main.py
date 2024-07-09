dfdbe8c91d2d253aa7f9086233c9067280e3455f
pavlakisjanina <160166957+pavlakisjanina@users.noreply.github.com>
2024-07-09 21:17:08 +0000

    Day 2: Tip Calculator

---
+++ main.py
@@ -0,0 +1,6 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+meal_with_tip = (150/5) * 1.12
+#Format the result to 2 decimal places
+round(meal_with_tip, 2)
+#Tip: There are 2 ways to round a number.
+print(f"Each Person has to pay: {meal_with_tip}")
